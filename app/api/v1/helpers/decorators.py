6fb8dc7aac41606cf03443c5b9dd544562e2b9d4
muazzama <muazzama.anwar@yahoo.com>
2020-03-13 13:27:40 +0500

    fixes

--- app/api/v1/helpers/decorators.py
+++ app/api/v1/helpers/decorators.py
@@ -7,7 +7,7 @@ def restricted(f):
     @wraps(f)
     def user_level(self, tracking_id, **kwargs):
         case = Case.get_case(tracking_id)
-        if case['creator']['user_id'] != kwargs.get('status_args').get('user_id') and kwargs.get('status_args').get('role') == "staff":
+        if case and case['creator']['user_id'] != kwargs.get('status_args').get('user_id') and kwargs.get('status_args').get('role') == "staff":
             abort(401, description="Unauthorized Access")
         return f(self, tracking_id=tracking_id, **kwargs)
 
